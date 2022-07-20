86eab7e195eec755c03233bd31d358a39bf1b797
Nick Orlov <ialwaysreply@gmail.com>
2022-07-20 10:05:57 -0700

    Create Form component

---
+++ src/Form/Form.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      numberOfGuests: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+        
+      </div>
+    )
+  }
+
+}
