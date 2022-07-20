a5c769c1771b4812b568f0891d6d21b0da5bc401
Nick Orlov <ialwaysreply@gmail.com>
2022-07-20 10:26:46 -0700

    Finish the forms

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -17,12 +17,33 @@ class Form extends Component {
       name: event.target.value
     })
   }
+  displayDate = (event) => {
+    this.setState({
+      // ...this.state,
+      date: event.target.value
+    })
+  }
+  displayTime = (event) => {
+    this.setState({
+      // ...this.state,
+      time: event.target.value
+    })
+  }
+  displayNumberOfGuests = (event) => {
+    this.setState({
+      // ...this.state,
+      numberOfGuests: event.target.value
+    })
+  }
 
   render() {
     console.log('this is Form state', this.state)
     return (
       <div>
         <input type='text' placeholder='Name' onInput={(event) => this.displayName(event)}/>
+        <input type='text' placeholder='Date' onInput={(event) => this.displayDate(event)}/>
+        <input type='text' placeholder='Time' onInput={(event) => this.displayTime(event)}/>
+        <input type='text' placeholder='Number of guests' onInput={(event) => this.displayNumberOfGuests(event)}/>
 
       </div>
     )
